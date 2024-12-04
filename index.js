b70a0e29421888e7b6bb36c58bb8dfbe4967c79e
mrhl082024 <mrhl082024@gmail.com>
2024-12-04 14:43:10 +0100

    laget healthbar

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const restartButton = document.getElementById("restart-button");
 const actionBar = document.getElementById("action-bar");
+const navbar = document.getElementById("navbar");
+const healthBar = document.getElementById("health-bar");
 
 function createAttackButton() {
   let attackButton = document.createElement("button");
@@ -7,3 +9,11 @@ function createAttackButton() {
   actionBar.appendChild(attackButton);
 }
 createAttackButton();
+
+function createHealthBar() {
+  let health = document.createElement("progress");
+
+  health.value = 100;
+  healthBar.appendChild(health);
+}
+createHealthBar();
