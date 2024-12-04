31bc49e09277b52772c040db048d9190aa491e1e
mrhl082024 <mrhl082024@gmail.com>
2024-12-04 12:20:05 +0100

    first

---
+++ index.js
@@ -0,0 +1,3 @@
+const attackButton = document.getElementById("attack-button");
+const defendButton = document.getElementById("defen-button");
+const restartButton = document.getElementById("restart-button");
