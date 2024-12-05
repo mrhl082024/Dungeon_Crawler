ea318c109ef91eed7ad0a9bf066c53dc7569e5a3
mrhl082024 <mrhl082024@gmail.com>
2024-12-05 13:57:15 +0100

    laget button function

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
-const restartButton = document.getElementById("restart-button");
 const actionBar = document.getElementById("action-bar");
-const navbar = document.getElementById("navbar");
-const healthBar = document.getElementById("health-bar");
 
-function createAttackButton() {
-  let attackButton = document.createElement("button");
-  attackButton.textContent = "Attack";
-  actionBar.appendChild(attackButton);
-}
-createAttackButton();
+const startBtn = document.getElementById("start-button");
 
-function createHealthBar() {
-  let health = document.createElement("progress");
-
-  health.value = 100;
-  healthBar.appendChild(health);
-}
-createHealthBar();
+//gjør "Start" om til "Attack!"
+startBtn.addEventListener("click", (e) => {
+  startBtn.textContent = "Attack!";
+});
